2f6b16d5946f4df3a93801f76ec2326f077f35b5
Reza Salehi <zaalion@gmail.com>
2018-08-20 22:23:58 -0400

    Adding MSI

---
+++ MyAddressBook+/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.KeyVault;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MyAddressBookPlus
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(KeyVaultService.GetToken));
+            var sec = kv.GetSecretAsync(WebConfigurationManager.AppSettings["CacheConnectionSecretUri"]).Result;
+            KeyVaultService.CacheConnection = sec.Value;
+        }
+    }
+}
---
+++ MyAddressBook+/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace MyAddressBookPlus.Models
+{
+    /// <summary>
+    /// Contact view model which is used in the controller and views
+    /// </summary>
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string PictureName { get; set; }
+    }
+}
