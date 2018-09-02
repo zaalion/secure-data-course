7bb8f4029ab05170784bccb9e95e072d3face01e
Reza Salehi <zaalion@gmail.com>
2018-09-02 00:32:28 -0400

    Revert "bs"
    This reverts commit 5228e0186b14c65cab9ca0a66dba576102469a6d.

---
+++ MyAddressBook+/AlwaysEncryptedInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.SqlServer.Management.AlwaysEncrypted.AzureKeyVaultProvider;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyAddressBookPlus
+{
+    public class AlwaysEncryptedInitializer
+    {
+        private static ClientCredential _clientCredential;
+
+        public static void InitializeAzureKeyVaultProvider()
+        {
+            var applicationId = ConfigurationManager.AppSettings["ClientId"].ToString();
+            var clientKey = ConfigurationManager.AppSettings["ClientSecret"].ToString();
+
+            _clientCredential = new ClientCredential(applicationId, clientKey);
+
+            SqlColumnEncryptionAzureKeyVaultProvider azureKeyVaultProvider =
+               new SqlColumnEncryptionAzureKeyVaultProvider(GetToken);
+
+            Dictionary<string, SqlColumnEncryptionKeyStoreProvider> providers =
+               new Dictionary<string, SqlColumnEncryptionKeyStoreProvider>();
+
+            providers.Add(SqlColumnEncryptionAzureKeyVaultProvider.ProviderName, azureKeyVaultProvider);
+            SqlConnection.RegisterColumnEncryptionKeyStoreProviders(providers);
+        }
+
+        public async static Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, _clientCredential);
+
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the access token");
+            return result.AccessToken;
+        }
+    }
+}
